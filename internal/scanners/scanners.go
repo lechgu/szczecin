8ce2d44f6d74ed15a2a71b802771cd1111349b8b
Lech Gudalewicz <lechgu@gmail.com>
2024-02-11 12:03:22 -0800

    string request

--- internal/scanners/scanners.go
+++ internal/scanners/scanners.go
@@ -1,24 +1,26 @@
 package scanners
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
 
 func Scan(minPort uint16, maxPort uint16, host string, workers int, timeout time.Duration) <-chan string {
 
-	requests := make(chan uint16, workers)
+	requests := make(chan string, workers)
 	results := make(chan string)
 	var wg sync.WaitGroup
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		work(&wg, host, timeout, requests, results)
+		work(&wg, timeout, requests, results)
 	}
 
 	go func() {
 		for i := minPort; i <= maxPort; i++ {
-			requests <- i
+			address := fmt.Sprintf("%s:%d", host, i)
+			requests <- address
 		}
 		close(requests)
 		wg.Wait()
--- internal/scanners/work.go
+++ internal/scanners/work.go
@@ -1,17 +1,15 @@
 package scanners
 
 import (
-	"fmt"
 	"net"
 	"sync"
 	"time"
 )
 
-func work(wg *sync.WaitGroup, host string, timeout time.Duration, ports <-chan uint16, results chan<- string) {
+func work(wg *sync.WaitGroup, timeout time.Duration, adresses <-chan string, results chan<- string) {
 	go func() {
 		defer wg.Done()
-		for port := range ports {
-			address := fmt.Sprintf("%s:%d", host, port)
+		for address := range adresses {
 			conn, err := net.DialTimeout("tcp", address, timeout)
 			if err != nil {
 				results <- ""
