9be211fcaced774d9e0a75683e17e133b65a83cb
Lech Gudalewicz <lechgu@gmail.com>
2024-02-10 17:34:38 -0800

    output

--- internal/commands/root_cmd.go
+++ internal/commands/root_cmd.go
@@ -1,70 +1,16 @@
 package commands
 
-import (
-	"fmt"
-	"sort"
-	"time"
-
-	"github.com/lechgu/szczecin/internal/scanners"
-	"github.com/samber/lo"
-	"github.com/schollz/progressbar/v3"
-	"github.com/spf13/cobra"
-)
-
-var (
-	host     string
-	minPort  uint16
-	maxPort  uint16
-	workers  int
-	timeout  int
-	progress bool
-)
+import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
 	Use:   "szczecin",
 	Short: "Port scanner",
-	RunE:  scan,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
 }
 
 func Execute() {
 	err := rootCmd.Execute()
 	cobra.CheckErr(err)
-}
-
-func scan(cmd *cobra.Command, args []string) error {
-	results := scanners.Scan(minPort, maxPort, host, workers, time.Second*time.Duration(timeout))
-	var bar *progressbar.ProgressBar
-	if progress {
-		bar = progressbar.Default(int64(maxPort - minPort + 1))
-	}
-	var ports []uint16
-	i := 0
-	for port := range results {
-
-		ports = append(ports, port)
-		i++
-		if progress {
-			_ = bar.Add(1)
-		}
-	}
-	ports = lo.Filter(ports, func(item uint16, _ int) bool {
-		return item != 0
-	})
-	sort.Slice(ports, func(i, j int) bool {
-		return ports[i] < ports[j]
-	})
-	lo.ForEach(ports, func(item uint16, _ int) {
-		fmt.Println(item)
-	})
-	return nil
-}
-
-func init() {
-	rootCmd.Flags().StringVar(&host, "host", "", "Host to scan")
-	rootCmd.MarkFlagRequired("host")
-	rootCmd.Flags().Uint16Var(&minPort, "min-port", 1, "starting port")
-	rootCmd.Flags().Uint16Var(&maxPort, "max-port", 1024, "ending port")
-	rootCmd.Flags().IntVar(&workers, "workers", 16, "Numbers of concurrent workers")
-	rootCmd.Flags().IntVar(&timeout, "timeout", 10, "Connection timeout, in seconds")
-	rootCmd.Flags().BoolVar(&progress, "progress", false, "Show progress")
 }
---
+++ internal/commands/scan_cmd.go
@@ -0,0 +1,66 @@
+package commands
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/lechgu/szczecin/internal/scanners"
+	"github.com/samber/lo"
+	"github.com/schollz/progressbar/v3"
+	"github.com/spf13/cobra"
+)
+
+var (
+	host     string
+	minPort  uint16
+	maxPort  uint16
+	workers  int
+	timeout  int
+	progress bool
+)
+
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Scan the host for open ports",
+	RunE:  scan,
+}
+
+func scan(cmd *cobra.Command, args []string) error {
+	results := scanners.Scan(minPort, maxPort, host, workers, time.Second*time.Duration(timeout))
+	var bar *progressbar.ProgressBar
+	if progress {
+		bar = progressbar.Default(int64(maxPort - minPort + 1))
+	}
+	var ports []uint16
+	i := 0
+	for port := range results {
+
+		ports = append(ports, port)
+		i++
+		if progress {
+			_ = bar.Add(1)
+		}
+	}
+	ports = lo.Filter(ports, func(item uint16, _ int) bool {
+		return item != 0
+	})
+	sort.Slice(ports, func(i, j int) bool {
+		return ports[i] < ports[j]
+	})
+	lo.ForEach(ports, func(port uint16, _ int) {
+		fmt.Printf("%s:%d\n", host, port)
+	})
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+	scanCmd.Flags().StringVar(&host, "host", "", "Host to scan")
+	scanCmd.MarkFlagRequired("host")
+	scanCmd.Flags().Uint16Var(&minPort, "min-port", 1, "starting port")
+	scanCmd.Flags().Uint16Var(&maxPort, "max-port", 1024, "ending port")
+	scanCmd.Flags().IntVar(&workers, "workers", 16, "Numbers of concurrent workers")
+	scanCmd.Flags().IntVar(&timeout, "timeout", 10, "Connection timeout, in seconds")
+	scanCmd.Flags().BoolVar(&progress, "progress", false, "Show progress")
+}
